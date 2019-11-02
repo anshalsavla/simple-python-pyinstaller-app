0003ef9f5091d6fbda0fbde9c77b7617b87880ed
Anshal Savla <asavla@resurety.com>
2019-11-02 14:22:18 -0400

    added shebang

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -l
 python3 -m venv jenkins_env
 source jenkins_env/bin/activate
 python -m py_compile sources/add2vals.py sources/calc.py
--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -l
 source jenkins_env/bin/activate
 pip install pytest
 py.test --verbose --junit-xml test-reports/results.xml sources/test_calc.py
